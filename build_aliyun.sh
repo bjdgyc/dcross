0e1dc26747cb75c5b397f728914c577dd2eb7e01
bjdgy <bjdgy@163.com>
2024-01-26 22:10:24 +0800

    init

--- build_aliyun.sh
+++ build_aliyun.sh
@@ -3,7 +3,7 @@
 #docker login -u bjdgyc
 ver=$(cat version)
 
-server="docker.io"
+server="registry.cn-qingdao.aliyuncs.com"
 
 #> build.log 2>&1
 docker buildx build -t $server/bjdgyc/dcross --progress=plain \
@@ -11,7 +11,7 @@ docker buildx build -t $server/bjdgyc/dcross --progress=plain \
 
 docker tag $server/bjdgyc/dcross:latest $server/bjdgyc/dcross:$ver
 
-exit 0
+#exit 0
 
 docker push $server/bjdgyc/dcross:$ver
 docker push $server/bjdgyc/dcross:latest
--- demo/main.go
+++ demo/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	fmt.Printf("build on %s [%s, %s]", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 }
