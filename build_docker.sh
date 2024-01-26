0e7aaef25c4e876562d61717029bc7bba0f2d0eb
bjdgyc <bjdgyc@163.com>
2024-01-26 18:04:39 +0800

    init

--- build_docker.sh
+++ build_docker.sh
@@ -3,13 +3,15 @@
 #docker login -u bjdgyc
 ver=v0.0.1
 
+server="registry.cn-qingdao.aliyuncs.com"
+
 #> build.log 2>&1
-docker buildx build -t registry.cn-hangzhou.aliyuncs.com/bjdgyc/dcross --progress=plain \
+docker buildx build -t $server/bjdgyc/dcross --progress=plain \
   --build-arg Commit=$(git rev-parse HEAD) --build-arg Ver=$ver .
 
-docker tag registry.cn-hangzhou.aliyuncs.com/bjdgyc/dcross:latest registry.cn-hangzhou.aliyuncs.com/bjdgyc/dcross:$ver
+docker tag $server/bjdgyc/dcross:latest $server/bjdgyc/dcross:$ver
 
 #exit 0
 
-docker push registry.cn-hangzhou.aliyuncs.com/bjdgyc/dcross:$ver
-docker push registry.cn-hangzhou.aliyuncs.com/bjdgyc/dcross:latest
+docker push $server/bjdgyc/dcross:$ver
+docker push $server/bjdgyc/dcross:latest
